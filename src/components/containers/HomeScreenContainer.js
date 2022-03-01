e15714c3c4071f6899d0d0b12f5cd63be39c970b
Gargi Thakur <gargi14thakur@gmail.com>
2022-02-28 19:01:42 -0800

    add react-native-dotenv  configuration for api key

--- src/components/containers/HomeScreenContainer.js
+++ src/components/containers/HomeScreenContainer.js
@@ -1,10 +1,15 @@
-import { Center, VStack } from "native-base";
+import { Center, VStack, Text } from "native-base";
 import React from "react";
+import { API_KEY } from "react-native-dotenv";
 
 const HomeScreenContainer = ({ data }) => {
   return (
     <VStack space={5} py={10} px={5}>
-      <Center>HOME SCREEN</Center>
+      <Center>HOME SCREEN
+        <Text>`${API_KEY}`</Text>
+
+      </Center>
+
     </VStack>
   );
 };
