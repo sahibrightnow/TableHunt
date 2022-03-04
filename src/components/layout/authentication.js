dc61e6785762b8459938f1ed36fb9c47c1e90c73
Sahibrightnow <jayess486@gmail.com>
2022-03-04 10:34:26 -0800

    cosmetic changes

--- src/components/layout/authentication.js
+++ src/components/layout/authentication.js
@@ -3,7 +3,8 @@ import * as Google from 'expo-google-app-auth'
 import * as WebBrowser from 'expo-web-browser';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
-import { Heading, Button, VStack, Flex, Link, Box} from 'native-base'
+import { Heading, Button, VStack, Flex, Link, Box} from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 const authentication = () => {
@@ -60,16 +61,20 @@ function showUserInfo() {
         <Flex alignItems="flex-start">
             <View style={styles.container}>
               {showUserInfo()}
-              <Heading>Start Table Hunting!</Heading>
+              <Heading style={styles.tableHunt}>Table Hunt</Heading>
+              <Text style={styles.subheading}>Choose your seating in seconds for your next occasion</Text>
+              <Text style={styles.subtitle}>You signup, we reserve. Quick!</Text>
               <Button borderRadius={8} width='50%' mt='5' >Continue with email</Button>
-              <Button borderRadius={8} width='50%' mt='5' onPress={accessToken ? getUserData : signInWithGoogleAsync}>
-              {accessToken ? "Get user data" : "Continue with google"}
+              
+              <Button  style={styles.button} borderRadius={8} width='50%' mt='5' onPress={accessToken ? getUserData : signInWithGoogleAsync}>
+              <MaterialCommunityIcons name="google" size={24} color="black"  />
+              <Text>{accessToken ? "Get user data" : "Continue with Google"}</Text>
               </Button>
               <Text>Not a member?<Link>Sign up</Link></Text>
               <StatusBar style="auto" />
              
             <Text>Are you a restaurant owner?</Text>
-            <Button borderRadius={8} width='50%' mt='5'  >Continue with email</Button>
+            <Button borderRadius={8} width='50%' mt='5'>Continue with email</Button>
               <Button borderRadius={8} width='50%' mt='5'  onPress={accessToken ? getUserData : signInWithGoogleAsync}>
               {accessToken ? "Get user data" : "Continue with google"}
               </Button>
@@ -105,6 +110,48 @@ const styles = StyleSheet.create({
     button: {
         borderRadius: 50,
         width: '100%',
+    },
+    tableHunt: {
+      width: 164,
+      height: 23,
+      color: 'rgb(106,153,78)',
+      textAlign: 'center',
+      fontSize: 30,
+    },
+    subheading: {
+      width: 367,
+      height: 108,
+      fontFamily: "Poppins",
+      fontSize: 24,
+      fontWeight: "600",
+      fontStyle: "normal",
+      letterSpacing: 0,
+      textAlign: "center",
+      color: 'rgb(20,17,15)'
+    },
+    subtitle: {
+      width: 216,
+      height: 21,
+      fontFamily: "Poppins",
+      fontSize: 14,
+      fontWeight: "normal",
+      fontStyle: "normal",
+      letterSpacing: 0,
+      textAlign: "center",
+      color: 'rgb(156,163,175)'
+    },
+    button: {
+      width: 335,
+  height: 48,
+  borderRadius: 4,
+  backgroundColor: 'rgb(255,255,255)',
+  shadowColor: "rgba(156, 163, 175, 0.4)",
+  shadowOffset: {
+    width: 0,
+    height: 4
+  },
+  shadowRadius: 12,
+  shadowOpacity: 1
     }
   });
 
