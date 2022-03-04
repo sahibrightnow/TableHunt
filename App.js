36107c39b1e3ff0328633ca3e3cd47503f6f1ed1
Harmandeep Singh <hsinghanandi@gmail.com>
2022-03-04 10:20:14 -0800

    fix: auth login on dev branch

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image, Button } from 'react-native'
 import { NativeBaseProvider } from 'native-base'
 import AppStack from './src/components/stacks/AppStack'
 import Authentication from './src/components/layout/authentication'
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
+
 const App = () => {
   return (
     <NativeBaseProvider>
-      <AppStack />
-      <StatusBar style="light" />
-      {/* <View style={styles.container}>
-        <Authentication/>
+      {/* <AppStack /> */}
+      {/* <StatusBar style="light" /> */}
+      <View style={styles.container}>
+        <Authentication />
         <StatusBar style="auto" />
-      </View> */}
+      </View>
     </NativeBaseProvider>
   )
 }
