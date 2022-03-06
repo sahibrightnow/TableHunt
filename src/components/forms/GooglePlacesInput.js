261a0ddd4903a3830b905efe414a8f5ac86873e9
Gargi Thakur <gargi14thakur@gmail.com>
2022-03-06 00:45:05 -0800

    add HomeScreen google autocomplete in input

--- src/components/forms/GooglePlacesInput.js
+++ src/components/forms/GooglePlacesInput.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Center, VStack, Text } from "native-base";
 import Constants from 'expo-constants';
 import { API_KEY } from "react-native-dotenv";
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
@@ -13,33 +14,32 @@ const GooglePlacesInput = () => {
 
     return (
 
-        <View style={styles.container}>
-            <GooglePlacesAutocomplete
-                placeholder="Search"
-                query={{
-                    key: API_KEY,
-                    language: 'en', // language of the results
-                }}
-                onPress={(data, details = null) => console.log(data)}
-                onFail={(error) => console.error(error)}
-                requestUrl={{
-                    url:
-                        'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
-                    useOnPlatform: 'web',
-                }} // this in only required for use on the web. See https://git.io/JflFv more for details.
-            />
-        </View>
+        <GooglePlacesAutocomplete
+            placeholder="Search"
+            query={{
+                key: API_KEY,
+                language: 'en', // language of the results
+            }}
+            onPress={(data, details = null) => console.log(data)}
+            onFail={(error) => console.error(error)}
+            requestUrl={{
+                url:
+                    'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api',
+                useOnPlatform: 'web',
+            }} // this in only required for use on the web. See https://git.io/JflFv more for details.
+        />
+
     );
 };
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-        paddingTop: Constants.statusBarHeight + 10,
-        backgroundColor: '#ecf0f1',
-        height: '100%',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         padding: 10,
+//         paddingTop: Constants.statusBarHeight + 10,
+//         backgroundColor: '#ecf0f1',
+//         height: '100%',
+//     },
+// });
 
 export default GooglePlacesInput;
