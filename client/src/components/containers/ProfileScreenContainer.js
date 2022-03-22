7265531218c9510f15f0297415ee4d95d54d19e1
Sahibrightnow <jayess486@gmail.com>
2022-03-21 17:24:16 -0700

    profile screen working

--- client/src/components/containers/ProfileScreenContainer.js
+++ client/src/components/containers/ProfileScreenContainer.js
@@ -1,10 +1,14 @@
-import { Center, VStack, Image, Text } from "native-base";
+import { Center, VStack, Image, Text, Heading, Divider, Button, ChevronRightIcon, HStack } from "native-base";
 import React, {useContext, useState, useEffect} from "react";
+import { StyleSheet } from 'react-native';
 import { LoginContext } from "../context/LoginContext";
+import { useNavigation } from '@react-navigation/native';
+
 
 const ProfileScreenContainer = () => {
   const [userInfo, setUserInfo] = useState();
-  const [accessToken] = useContext(LoginContext)
+  const [accessToken, setAccessToken] = useContext(LoginContext)
+  const navigation = useNavigation();
   async function getUserData() {
     let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
       headers: { 'Authorization': `Bearer ${accessToken}` }
@@ -18,11 +22,39 @@ const ProfileScreenContainer = () => {
     getUserData();
   }, userInfo)
 
+  const logOut = () => {
+    setAccessToken("");
+    navigation.navigate("Authentication");
+
+  }
+
+  const icon = [{ icon: <ChevronRightIcon size="4" />,
+  iconName: "chevron-right"}]
+
   return (
     <VStack space={5} py={10} px={5}>
       {userInfo ? (
-        <Center><Text>Hello {userInfo.name}</Text>
-        <Image source={{ uri: userInfo.picture }} width="100" height="100" />
+        <Center>
+        <Image source={{ uri: userInfo.picture }} style={styles.picture} />
+        <Heading fontSize="md" style={styles.text}>{userInfo.name} </Heading>
+        <VStack style={styles.options} width='100%'>
+          <HStack>
+            <Text fontSize="3xl" fontWeight="light">Account Settings</Text>
+            <ChevronRightIcon mt="2"  ml="auto"/>
+          </HStack>
+          <Divider my='2' />
+          <HStack>
+            <Text fontSize="3xl" fontWeight="light">Help & Support</Text>
+            <ChevronRightIcon mt="2" ml="auto"/>
+          </HStack>
+          <Divider my='2' />
+          <HStack>
+            <Text fontSize="3xl" fontWeight="light">Terms & Privacy</Text>
+            <ChevronRightIcon mt="2"  ml="auto"/>
+          </HStack>
+          <Divider my='2' />
+          <Button size="lg" mt="220" backgroundColor="#924344" borderRadius="md" onPress={() => logOut()}>Log Out</Button>
+        </VStack>
         </Center>
       ) : <Text>Loading</Text>}
       
@@ -31,3 +63,21 @@ const ProfileScreenContainer = () => {
 };
 
 export default ProfileScreenContainer;
+
+const styles = StyleSheet.create({
+  picture: { 
+    width: 120, 
+    height: 120, 
+    borderRadius: 100,
+    marginTop: 50, 
+  },
+  text : { 
+    marginTop:20,
+    fontSize: 20,
+    fontWeight: 'bold' 
+  },
+  options: {
+    marginTop: 50,
+    width: 300,
+  }
+});
