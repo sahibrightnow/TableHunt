b036acbfc70324ab9d11831a419018a5eabdca22
Gargi Thakur <gargi14thakur@gmail.com>
2022-04-07 03:59:06 -0700

    style: darken skeleton color

--- client/src/components/listitems/SearchPageSkeletonCard.js
+++ client/src/components/listitems/SearchPageSkeletonCard.js
@@ -4,15 +4,15 @@ const SearchPageSkeletonCard = () => {
     return (
         <Center w="100%">
             <HStack w="95%" maxW="400" borderWidth="1" space={4} rounded="md" _dark={{
-                borderColor: "coolGray.500"
+                borderColor: "coolGray.700"
             }} _light={{
-                borderColor: "coolGray.200"
+                borderColor: "coolGray.300"
             }} p="2">
-                <Skeleton flex="1" h="145" rounded="md" startColor="coolGray.100" />
+                <Skeleton flex="1" h="145" rounded="md" startColor="coolGray.400" />
                 <VStack space="2" flex="2">
                     <Skeleton h="6" rounded="full" />
                     <Skeleton.Text />
-                    <Skeleton h="7" startColor="green.100" />
+                    <Skeleton h="7" startColor="green.200" />
                 </VStack>
             </HStack>
         </Center>
--- client/src/components/listitems/SkeletonCard.js
+++ client/src/components/listitems/SkeletonCard.js
@@ -4,9 +4,9 @@ const SkeletonCard = () => {
     return (
 
         <VStack mt="3" w="84%" maxW="305" height="320" borderWidth="1" space={4} overflow="hidden" rounded="md" _dark={{
-            borderColor: "coolGray.600"
+            borderColor: "coolGray.700"
         }} _light={{
-            borderColor: "coolGray.200"
+            borderColor: "coolGray.300"
         }}>
             <Skeleton h="195" />
             <Skeleton.Text px="4" />
